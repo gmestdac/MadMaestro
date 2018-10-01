2e96675f17790bfce000a66f412a797b7d1a7672
Gianny Mestdach <giannymestdach@gmail.com>
2018-10-01 15:29:00 +0200

    mg config automatically set by setup

--- setup.sh
+++ setup.sh
@@ -16,13 +16,22 @@ tar -xf dim6top_LO_UFO.tar.gz
 rm dim6top_LO_UFO.tar.gz
 cd..
 cd.. 
+tar -xf ./templates/modHEL_UFO.tar.gz -C ./MG5_aMC_v2_6_3_2/models/
 wget http://www.hepforge.org/archive/lhapdf/LHAPDF-6.2.1.tar.gz
 tar xf LHAPDF-6.2.1.tar.gz
 rm LHAPDF-6.2.1.tar.gz
 cd LHAPDF-6.2.1 
-echo -n "Installing LHAPDF, good time grab a coffee"
+echo -e '\033[1m\n\nInstalling LHAPDF, this a good time grab a coffee \n\n\033[0m'
 ./configure --prefix="$location"/LHAPDF
 make
 make install
 cd ..
 rm -rf LHAPDF-6.2.1 
+# 
+lhapdfpath=$(readlink -e ./LHAPDF/bin/lhapdf-config) 
+sed "s|TOBEREPLACEDBYSCRIPT| lhapdf = $lhapdfpath|g" ./templates/mg5_configuration.txt > ./MG5_aMC_v2_6_3_2/input/mg5_configuration.txt
+# making the standard set restrict files
+cd scripts
+python HELresscript.py
+python dim6resscript.py
+cd ..
